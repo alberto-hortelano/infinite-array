050e911123efc1d3ab5daea99858ace841e9b647
Alberto Hortelano <alberto.hortelano.moran@gmail.com>
2016-10-08 15:30:14 +0200

    Create infinite-array.js

---
+++ infinite-array.js
@@ -0,0 +1,30 @@
+
+		/** 
+		 * Infinite array
+		 */
+		function inifiniteArray(length, original) {
+			var obj = new Array();
+			obj.length = length;
+
+			if (original !== undefined) {
+				obj.original = original;
+			} else {
+				obj.original = obj;
+			}
+
+			return new Proxy(obj, {
+			    get(target,name) {
+			        if (target.hasOwnProperty(name)) {
+			        	return target[name];
+			        } else if (!randomIntFromInterval(0,3)) {
+			        	var newLength = randomIntFromInterval(1,length);
+						var ret = inifiniteArray(newLength, obj.original);
+						return ret;
+					} else {
+						var n = randomIntFromInterval(1,length);
+						return n;
+					}
+			    }
+			});
+		}
+    inifiniteArray(10);
