29198cef59cf1439495ca84e2bd102aaabd059b2
Alberto Hortelano <alberto.hortelano.moran@gmail.com>
2016-10-08 15:32:42 +0200

    Update infinite-array.js

--- infinite-array.js
+++ infinite-array.js
@@ -1,30 +1,30 @@
 
-		/** 
-		 * Infinite array
-		 */
-		function inifiniteArray(length, original) {
-			var obj = new Array();
-			obj.length = length;
+/** 
+ * Infinite array
+ */
+function inifiniteArray(length, original) {
+	var obj = new Array();
+	obj.length = length;
 
-			if (original !== undefined) {
-				obj.original = original;
+	if (original !== undefined) {
+		obj.original = original;
+	} else {
+		obj.original = obj;
+	}
+
+	return new Proxy(obj, {
+	    get(target,name) {
+		if (target.hasOwnProperty(name)) {
+			return target[name];
+		} else if (!randomIntFromInterval(0,3)) {
+			var newLength = randomIntFromInterval(1,length);
+				var ret = inifiniteArray(newLength, obj.original);
+				return ret;
 			} else {
-				obj.original = obj;
+				var n = randomIntFromInterval(1,length);
+				return n;
 			}
-
-			return new Proxy(obj, {
-			    get(target,name) {
-			        if (target.hasOwnProperty(name)) {
-			        	return target[name];
-			        } else if (!randomIntFromInterval(0,3)) {
-			        	var newLength = randomIntFromInterval(1,length);
-						var ret = inifiniteArray(newLength, obj.original);
-						return ret;
-					} else {
-						var n = randomIntFromInterval(1,length);
-						return n;
-					}
-			    }
-			});
-		}
-    inifiniteArray(10);
+	    }
+	});
+}
+inifiniteArray(10);
